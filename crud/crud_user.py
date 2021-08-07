f178ff1b47dc1816701e6bbe0242fe37178cfab3
최태훈 <taehoon@choetaehun-ui-MacBookPro.local>
2021-08-07 15:12:09 +0900

    make create_user

--- crud/crud_user.py
+++ crud/crud_user.py
@@ -1,23 +1,30 @@
 from datetime import date
 from typing import List
 from sqlalchemy.orm import Session
-from models.base import user,part,position
+from models.base import *
 
-def get_multi(*,db:Session) -> List[User]:
-    return db.query(user,part,position).join(part,position)
 
-def create_user(*,db:Session):
-    if db.query(user).fileter(user.name == user_name).first():
-        return False
-    else:
-        db.obj = user(
-            name=user_name,
-            position = user_position,
-            part = user_part,
+
+def create_user(
+    *,
+    db : Session,
+    name:str,
+    position:str,
+    part:str,
+    start_date):
+
+    if db.query(User).filter(User.name == name).first()==None:
+        part_id=db.query(Part).filter(Part.part ==part).first().id
+        position_id = db.query(Position).filter(Position.position==position).first().id
+        new_user = User(
+            name=name,
+            position_id = position_id,
+            part_id = part_id,
             start_date = start_date,
-            status = True
+            status = True,
+            annual_day = 0
         )
-        db.add(db.obj)
+        db.add(new_user)
         db.commit()
-        return db_obj
+        return new_user
 
