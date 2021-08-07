059c4b571975df0cc8fd76c374cfbf63135a3546
최태훈 <taehoon@choetaehun-ui-MacBookPro.local>
2021-08-07 15:11:44 +0900

    회원가입

--- models/base.py
+++ models/base.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func, Bool
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
 import sys
 import os
 
@@ -12,24 +11,25 @@ from db.session import engine
 
 Base = declarative_base()
 
-class user(Base):
+class User(Base):
   __tablename__ = 'users'
   id = Column(Integer, primary_key=True, autoincrement=True, index=True)
   name = Column(String(20), unique=True)
   position_id = Column(Integer, ForeignKey("positions.id"))
-  position = relationship("position", foreign_keys=[position_id])
+  position = relationship('Position', foreign_keys=[position_id])
   part_id = Column(Integer, ForeignKey("parts.id"))
-  part = relationship("part", foreign_keys=[part_id])
+  part = relationship('Part', foreign_keys=[part_id])
   start_date = Column(Date)
   status = Column(Boolean)
+  annual_day = Column(Integer)
 
-class position(Base):
+class Position(Base):
   __tablename__ = 'positions'
   id = Column(Integer, primary_key=True, autoincrement=True, index=True)
   position = Column(String(20), unique=True)
 
 
-class part(Base):
+class Part(Base):
   __tablename__ = 'parts'
   id = Column(Integer, primary_key=True, autoincrement=True, index=True)
   part = Column(String(20), unique=True)
